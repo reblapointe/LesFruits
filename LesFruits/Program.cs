080e386d0f03c5598f8e2861773ec5d013e99d4d
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2024-02-27 10:44:02 -0500

    Solution

--- LesFruits/Program.cs
+++ LesFruits/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace LesFruits
 {
@@ -36,12 +38,70 @@ namespace LesFruits
                             Select(p => new { p.Age, p.Genre});
             Console.WriteLine($"Age et genre des hommes : {string.Join(separator: ", ", values: query)}");
 
-            Console.Read();
+            var enfants = LesEnfants(personnes);
+            Console.WriteLine($"Les enfants : {string.Join(separator: ", ", values: enfants)}");
 
+            Personne aine = LaPlusVieille(personnes);
+            Console.WriteLine($"La personne la plus âgée est : {aine}");
+
+            var pop = PlusPopulaire(personnes);
+            Console.WriteLine($"Les fruits les plus populaires : {string.Join(separator: ", ", values: pop)}");
+
+            ParGenre(personnes);
+
+            Console.Read();
+        }
+        public static IEnumerable<Fruit> PlusPopulaire(IEnumerable<Personne> personnes)
+        {
+            // return personnes.SelectMany(p => p.FruitsAimes).GroupBy(f => f).OrderByDescending(g => g.Count()).Select(g => g.Key);
+
+            return from p in personnes
+                   from f in p.FruitsAimes
+                   group f by f into fruits
+                   orderby fruits.Count() descending
+                   select fruits.Key;
+        }
+        public static IEnumerable<Personne> LesEnfants(IEnumerable<Personne> personnes)
+        {
+            // return personnes.Where(p => p.Age < 18).Select(p => p);
+            return from p in personnes where p.Age < 18 select p;
+        }
+
+        public static void ParGenre(IEnumerable<Personne> personnes)
+        {
+            /*var genres = personnes.GroupBy(p => p.Genre).Select(g => new {
+                key = g.Key, 
+                count = g.Count(), 
+                max = g.Max(p => p.Age), 
+                min = g.Min(p => p.Age) });*/
+
+            var genres = from p in personnes
+                         group p by p.Genre into g
+                         select new
+                         {
+                             key = g.Key,
+                             count = g.Count(),
+                             max = g.Max(p => p.Age),
+                             min = g.Min(p => p.Age)
+                         };
+
+            foreach (var g in genres)
+                Console.WriteLine($"Genre : {g.key}\n" +
+                    $"Nombre de personnes de ce genre : {g.count}\n" +
+                    $"L'âge de la personne la plus vieille de ce genre : {g.max}\n" +
+                    $"L'âge de la personne la plus jeune de ce genre : {g.min}\n");
+
+        }
+
+
+        public static Personne LaPlusVieille(IEnumerable<Personne> personnes)
+        {
+            // return (from p in personnes orderby p.Age select p).FirstOrDefault();
+            return personnes.OrderByDescending(p => p.Age).FirstOrDefault();
         }
         static bool AvecA(Fruit fruit)
         {
-            return fruit.Nom.ToUpper().Contains('A');
+            return fruit.Nom.ToUpper().Contains("A");
         }
     }
 }
